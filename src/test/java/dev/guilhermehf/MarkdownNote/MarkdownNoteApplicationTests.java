6ddd45b754c5a8e6dfe8051e42943bab1647c9ad
Guilherme <guilherme.fenrique@gmail.com>
2025-06-14 20:39:28 +0200

    first commit

---
+++ src/test/java/dev/guilhermehf/MarkdownNote/MarkdownNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilhermehf.MarkdownNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
